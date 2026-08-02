54f30ec2451e2047ce39816455a99ec236a3cf69
Mazloumh <mazloumh@hotmail.com>
2026-08-02 13:28:49 +0200

    knapp enter i topping

--- ball.js
+++ ball.js
@@ -1,29 +1,50 @@
 function rollBall() {
-  const numOfBall = document.getElementById("numOfBall").value;
+
+  const numOfBall = Number(document.getElementById("numOfBall").value);
   const ballResult = document.getElementById("ballResult");
   const ballImages = document.getElementById("ballImages");
+
   const values = [];
   const images = [];
 
-  while (values.length < numOfBall) {
-    const value = Math.floor(Math.random() * 48) + 1; // 1-48
-    if (values.indexOf(value) === -1) {
-      values.push(value);
-      images.push(`<img src="ball_images/${value}.png" alt="Ball ${value}">`);
-    }
-    
-    if (numOfBall > 7) {
-      alert(
-        `"Vikinglotto er et lotterispill i samarbeid mellom ti land, hvor det trekkes 6 av 48 hovedtall og 1 av 5 vikingtall .\n Det trekkes også ett tilleggstall blant de 48. Du nå trekket ${numOfBall} og det tallet er mer enn 7 hovedtall."`,
-      );
-      return;
-    }
 
-    ballResult.textContent = `Ball : ${values.join(" - ")}`;
-    ballImages.innerHTML = images.join("");
+  if (numOfBall > 7 || numOfBall <= 0) {
+    alert(
+      "Vikinglotto trekker 6 hovedtall fra 48. Du kan velge maksimalt 7 tall."
+    );
+    return;
   }
 
-  /*  for( let i = 0; i < numOfBall; i++){
-        const value = Math.floor(Math.random()*6) +1;
-        values.push(value); */
+
+  while (values.length < numOfBall) {
+
+    const value = Math.floor(Math.random() * 48) + 1;
+
+    if (!values.includes(value)) {
+      values.push(value);
+      images.push(
+        `<img src="ball_images/${value}.png" alt="Ball ${value}">`
+      );
+    }
+  }
+
+
+  ballResult.textContent = `Ball : ${values.join(" - ")}`;
+  ballImages.innerHTML = images.join("");
 }
+
+
+
+document.getElementById("numOfBall").addEventListener("keydown", function(e){
+
+  if(e.key === "Enter"){
+    rollBall();
+  }
+
+});
+
+document.getElementById("numOfBall").addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    rollBall();
+  }
+});
