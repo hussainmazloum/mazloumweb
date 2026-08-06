0f8b8ce430cc0731446574ed33c0a1a3189794bb
Mazloumh <mazloumh@hotmail.com>
2026-08-06 15:29:20 +0200

    juster tipping til norske utrykke

--- ball.js
+++ ball.js
@@ -1,50 +1,57 @@
 async function rollBall(maxNumber) { // maxNumber er det største tallet som kan trekkes.(lotto :34 viking : 48)
 
 
-  const numOfBall = Number(document.getElementById("numOfBall").value);// Avlesing av antall påkrevde baller.
-  const ballResult = document.getElementById("ballResult"); // For å vise de endelige tallene.
-  const ballImages = document.getElementById("ballImages"); // For å se bilder av ballene
+  const antallBall = Number(document.getElementById("antallBall").value);// Avlesing av antall påkrevde baller.
+  const ballResultat = document.getElementById("ballResultat"); // For å vise de endelige tallene.
+  const ballBilder = document.getElementById("ballBilder"); // For å se bilder av ballene
 
   const values = []; // Opprette en array med tall, (De uttrukne tallene vil bli lagret i den.)
 
 
-  ballResult.textContent = ""; // Fjern tidligere resultater.
-  ballImages.innerHTML = "";
+  ballResultat.textContent = ""; // Fjern tidligere resultater.
+  ballBilder.innerHTML = "";
 
 
-  if (numOfBall <= 0 || numOfBall > 7) {
+  if (antallBall <= 0 || antallBall > 7) {
     alert("Du kan velge maksimalt 7 tall."); // Kontroll av antall baller.
     return;
   }
 
+  //--------------------------------- Tilfeldig trekning --------------------------------------------------------------
 
-  while (values.length < numOfBall) { // Tilfeldig trekning ( Løkken fortsetter helt til antallet trukne sifre tilsvarer det påkrevde antallet.)
+  while (values.length < antallBall) { // Tilfeldig trekning ( Løkken fortsetter helt til antallet trukne sifre tilsvarer det påkrevde antallet.)
 
     const value = Math.floor(Math.random() * maxNumber) + 1; // Generer et tilfeldig tall. (avhenging av maxNumber)
+
+  //--------------------------------- Forebygging av duplisering -------------------------------------------------------
 
     if (!values.includes(value)) { // Forebygging av duplisering (منع التكرار)
 
       values.push(value); // Legge til tallet i array
 
-      ballImages.innerHTML +=
+      ballBilder.innerHTML +=
         `<img src="ball_images/${value}.png" alt="Ball ${value}" class="ball">`; // Vis bilde av ballen.
 
-      if (values.length < numOfBall) {
+//-------------------------------------- Forsinkelse mellom baller -------------------------------------------------------
+
+      if (values.length < antallBall) {
         await new Promise(resolve => setTimeout(resolve, 650)); // Forsinkelse mellom baller.
       }
     }
   }
 
 
-// Sortering av tallene etter trekningen
+//------------------------------------ Sortering av tallene etter trekningen ----------------------------------------
+
   values.sort((a, b) => a - b);
 
-  ballResult.textContent = `Tall : ${values.join(" - ")}`;
+  ballResultat.textContent = `Tall : ${values.join(" - ")}`;
 }
 
 
-// Enter-tasten fungerer avhengig av siden.
-const inputBall = document.getElementById("numOfBall"); // Hent inntastingsfeltet.
+//------------------------------------- Enter-tasten fungerer avhengig av siden.-------------------------------------
+
+const inputBall = document.getElementById("antallBall"); // Hent inntastingsfeltet.
 
 if (inputBall) { // Det verifiserer at elementet finnes på siden, for å forhindre en JavaScript-feil.
   inputBall.addEventListener("keydown", function(e) { // Det venter på at en tast trykkes i inndatafeltet.
@@ -64,5 +71,5 @@ if (inputBall) { // Det verifiserer at elementet finnes på siden, for å forhin
 
 //------------------------------------ enter knappen lotto --------------------------------------
 
-const lottoInputBall = document.getElementById("numOfBall");
+const lottoInputBall = document.getElementById("antallBall");
 
