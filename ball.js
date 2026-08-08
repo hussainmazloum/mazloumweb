a5307cda55f9e03e8c426f2d8ce818cce0be9b2d
Mazloumh <mazloumh@hotmail.com>
2026-08-08 15:52:58 +0200

    justere vikingo tall

--- ball.js
+++ ball.js
@@ -1,59 +1,87 @@
-async function rollBall(maxNumber) { // maxNumber er det største tallet som kan trekkes.(lotto :34 viking : 48)
+const verdier = [];
 
+// -----------------------------------------
+// 6 av 48
+// -----------------------------------------
 
-  const antallBall = Number(document.getElementById("antallBall").value);// Avlesing av antall påkrevde baller.
-  const ballResultat = document.getElementById("ballResultat"); // For å vise de endelige tallene.
-  const ballBilder = document.getElementById("ballBilder"); // For å se bilder av ballene
+async function rollBall(maxNumber) {
 
-  const verdier = []; // Opprette en array med tall, (De uttrukne tallene vil bli lagret i den.)
+  const antallBall = Number(
+    document.getElementById("antallBall").value
+  );
 
+  const ballResultat = document.getElementById("ballResultat");
+  const ballBilder = document.getElementById("ballBilder");
 
-  ballResultat.textContent = ""; // Fjern tidligere resultater.
+  ballResultat.textContent = "";
   ballBilder.innerHTML = "";
 
+  // Viking-ball finnes bare på Vikinglotto-siden
+  const vikingBallElement = document.getElementById("vikingBall");
+
+  if (vikingBallElement) {
+    vikingBallElement.innerHTML = "";
+  }
+
+  /* const verdier = []; */
+  verdier.length = 0; // Tøm tidligere tall
 
   if (antallBall <= 0 || antallBall > 7) {
-    /* alert("Du kan velge maksimalt 7 tall."); */ // Kontroll av antall baller.
 
-      Swal.fire({
+    Swal.fire({
       title: "Advarsel!",
-      width:300,
+      width: 300,
       text: "Du kan velge maksimalt 7 tall.",
       icon: "warning"
-      });
+    });
+
     return;
   }
 
-  //--------------------------------- Tilfeldig trekning --------------------------------------------------------------
+  while (verdier.length < antallBall) {
 
-  while (verdier.length < antallBall) { // Tilfeldig trekning ( Løkken fortsetter helt til antallet trukne sifre tilsvarer det påkrevde antallet.)
+    const value =
+      Math.floor(Math.random() * maxNumber) + 1;
 
-    const verdi = Math.floor(Math.random() * maxNumber) + 1; // Generer et tilfeldig tall. (avhenging av maxNumber)
+    if (!verdier.includes(value)) {
 
-  //--------------------------------- Forebygging av duplisering -------------------------------------------------------
+      verdier.push(value);
 
-    if (!verdier.includes(verdi)) { // Forebygging av duplisering (ikke gjenta den samme ballen)
+      ballBilder.innerHTML += `
+        <img src="ball_images/${value}.png"
+             alt="Ball ${value}"
+             class="ball">
+      `;
 
-      verdier.push(verdi); // Legge til tallet i array
-
-      ballBilder.innerHTML +=
-        `<img src="ball_images/${verdi}.png" alt="Ball ${verdi}" class="ball">`; // Vis bilde av ballen.
-
-//-------------------------------------- Forsinkelse mellom baller -------------------------------------------------------
-
-      if (verdier.length < antallBall) {
-        await new Promise(resolve => setTimeout(resolve, 650)); // Forsinkelse mellom baller.
-      }
+      await new Promise(resolve =>
+        setTimeout(resolve, 650)
+      );
     }
   }
 
-
-//------------------------------------ Sortering av tallene etter trekningen ----------------------------------------
-
   verdier.sort((a, b) => a - b);
 
-  ballResultat.textContent = `Tall : ${verdier.join(" - ")}`;
+  ballResultat.textContent =
+    `Tall : ${verdier.join(" - ")}`;
 }
+
+
+// -----------------------------------------
+// 1 av 5 Viking-ball
+// -----------------------------------------
+
+function rollVikingTall() {
+  const element = document.getElementById("vikingBall");
+
+  const vikingTall = Math.floor(Math.random() * 5) + 1;
+
+  element.innerHTML = ` Vikingotall er :
+    <img src="ball_images/${vikingTall}.png"
+         alt="Vikingtall ${vikingTall}"
+         class="ball">
+  `;
+}
+
 
 
 //------------------------------------- Enter-tasten fungerer avhengig av siden.-------------------------------------
